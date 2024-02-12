57bf99cbc6d2d0b0d7bcb9e22e524686dd29c291
cnh29 <Cameronharris94@gmail.com>
2024-02-12 12:38:12 +1100

    Updated version to 0.1.3 (#31)
    ShowPrintCrossdockLabel bug - default should be set to false
    Changed layout of LicensePlateScreen
    changed layout of LicensePlateSettingsScreen - not all content fit in view
    added validation to make sure handling unit was selected before adding license plate
    
    Co-authored-by: Cameron Harris <>

--- crossdock/src/main/java/io/dock2dock/android/crossdock/components/LicensePlateScreen.kt
+++ crossdock/src/main/java/io/dock2dock/android/crossdock/components/LicensePlateScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -83,77 +84,95 @@ fun LicensePlateContent(
     onReprint: (() -> Unit) = {},
     onViewLines: (() -> Unit) = {}
 ) {
+
+    val subTitle = if (licensePlate.ssccBarcode.isNullOrEmpty()) {
+        licensePlate.handlingUnitName
+    } else {
+        "${licensePlate.handlingUnitName} · ${licensePlate.ssccBarcode}"
+    }
+
     CompositionLocalProvider(LocalContentColor provides Color.White) {
-        Column(Modifier
-            .fillMaxWidth()
-            .zIndex(1f)
-            .background(color = PrimaryOxfordBlue)
-            .padding(horizontal = 8.dp, vertical = 4.dp),
-            verticalArrangement = Arrangement.spacedBy(4.dp)
+        Row {
+            Column(Modifier
+                .fillMaxWidth()
+                .zIndex(1f)
+                .background(color = PrimaryOxfordBlue)
+                .padding(start = 8.dp, end = 8.dp, top = 8.dp, bottom = 0.dp)
             ) {
-            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-                Text("Active License Plate - " + licensePlate.no, style = MaterialTheme.typography.subtitle2)
-                IconButton(modifier = Modifier.size(18.dp), onClick = {
-                    onClose()
-                }) {
-                    Icon(
-                        Icons.Filled.Close,
-                        "contentDescription")
-                }
-            }
-
-            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-                Column(modifier = Modifier.align(Alignment.Bottom)) {
-                    licensePlate.ssccBarcode?.let {
-                        Text(text = it, color = Color.LightGray, fontSize = 10.sp)
+                Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                    Column {
+                        Text(
+                            "Active License Plate - " + licensePlate.no,
+                            style = MaterialTheme.typography.subtitle2
+                        )
+                        Text(text = subTitle, color = Color.LightGray, fontSize = 10.sp)
                     }
-                    Text(licensePlate.handlingUnitName, fontSize = 10.sp)
-                }
-
-
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = licensePlate.totalCount.toString())
-                    Text("Lines", color = Color.LightGray, fontSize = 10.sp)
-                }
-
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = licensePlate.quantityDescription)
-                    Text("Quantity", color = Color.LightGray, fontSize = 10.sp)
-                }
-
-                Row(Modifier.align(Alignment.CenterVertically),
-                    horizontalArrangement = Arrangement.spacedBy(12.dp)) {
                     IconButton(modifier = Modifier.size(24.dp), onClick = {
-                        onRefresh()
+                        onClose()
                     }) {
                         Icon(
-                            Icons.Filled.Refresh,
+                            Icons.Filled.Close,
                             "contentDescription")
                     }
 
-                    IconButton(modifier = Modifier.size(24.dp), onClick = {
-                        onViewLines()
-                    }) {
-                        Icon(
-                            Icons.Filled.List,
-                            "contentDescription")
-                    }
-                    IconButton(modifier = Modifier.size(24.dp), onClick = {
-                        onComplete()
-                    }) {
-                        Icon(
-                            tint = Color.Green,
-                            imageVector = Icons.Filled.CheckCircle,
-                            contentDescription = "contentDescription")
+
+                }
+
+                Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+
+                    Row(horizontalArrangement = Arrangement.spacedBy(35.dp), modifier = Modifier.align(Alignment.CenterVertically)) {
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Text(text = licensePlate.totalCount.toString())
+                            Text("Lines", color = Color.LightGray, fontSize = 10.sp)
+                        }
+
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Text(text = licensePlate.quantityDescription)
+                            Text("Quantity", color = Color.LightGray, fontSize = 10.sp)
+                        }
                     }
 
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    Row(
+                        Modifier.align(Alignment.Bottom)
+                    ) {
+                        IconButton(onClick = {
+                            onRefresh()
+                        }) {
+                            Icon(
+                                Icons.Filled.Refresh,
+                                "contentDescription"
+                            )
+                        }
+
+                        IconButton(onClick = {
+                            onViewLines()
+                        }) {
+                            Icon(
+                                Icons.Filled.List,
+                                "contentDescription"
+                            )
+                        }
+                        IconButton(onClick = {
+                            onComplete()
+                        }) {
+                            Icon(
+                                tint = Color.Green,
+                                imageVector = Icons.Filled.CheckCircle,
+                                contentDescription = "contentDescription"
+                            )
+                        }
+
+                    }
                 }
             }
         }
+
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF, widthDp = 350)
 @Composable
 internal fun PreviewLicensePlateContent() {
     val licensePlate = LicensePlate("LP000008", "Chilled Chep", false, "000942190400001123456", Date(2023,12,4,12,22,0), 12.5, 2, 12)
